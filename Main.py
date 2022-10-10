7122593458f408b540e06ad978e00065f62a4b24
Marius R <marius.rekus@gmail.com>
2022-10-10 08:27:49 +0300

    Išimti widgets iš MainWindos, paimti iš Widgets

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import logging
 from Views import ProcessViews, RecipiesViews, StorageViews, OrderViews, MaterialsViews
 import API
 from datetime import datetime
+import widgets
 
 
 class MyButton(tk.Button):
@@ -135,7 +136,7 @@ class MyTreeview(ttk.Treeview):
         )
 
 
-class MainWindow(ProcessViews, RecipiesViews, StorageViews, OrderViews, MaterialsViews):
+class MainWindow(ProcessViews, RecipiesViews, StorageViews, OrderViews, MaterialsViews, widgets.Widgets):
     """
     Pagrindinis programos langas su bendrais
     metodais valdančiais visus langus bendrai
@@ -145,67 +146,6 @@ class MainWindow(ProcessViews, RecipiesViews, StorageViews, OrderViews, Material
         super().__init__()
 
         self.master = master
-        self.topFrame = None
-        self.leftFrame = None
-
-        self.materialsTable = None
-        self.storageTable = None
-        self.recipeTable = None
-        self.ordersTable = None
-        self.processTable = None
-
-        self.labelEdit1 = None
-        self.labelEdit2 = None
-        self.labelEdit3 = None
-        self.labelEdit4 = None
-        self.labelEdit5 = None
-        self.labelEdit6 = None
-        self.labelFilterFrom = None
-        self.labelFilterTo = None
-        self.labelOrderProfit1 = None
-        self.labelOrderProfit2 = None
-        self.labelOrder = None
-        self.labelRecipe = None
-        self.labelMailOrder = None
-
-        self.entryFieldEdit1 = None
-        self.entryFieldEdit2 = None
-        self.entryFieldEdit3 = None
-        self.entryFieldEdit4 = None
-        self.entryFieldEdit5 = None
-        self.entryFieldEdit6 = None
-        self.entryFieldOrder = None
-
-        self.buttonAddRecipe = None
-        self.buttonEdit = None
-        self.buttonDelete = None
-        self.buttonAddOrder = None
-        self.buttonConfirmOrder = None
-        self.buttonCancelOrder = None
-        self.buttonFilterOrders = None
-        self.buttonCancelEditing = None
-        self.buttonMenu1 = None
-        self.buttonMenu2 = None
-        self.buttonMenu3 = None
-        self.buttonScrollUp = None
-        self.buttonScrollDown = None
-
-        self.idForEdit = None
-        self.scrollbar_recipe = None
-        self.rates = None
-        self.pairs = None
-        self.currency_list = None
-        self.recipe_list = None
-        self.mail_list = None
-        self.year_list_from = None
-        self.month_list_from = None
-        self.day_list_from = None
-        self.year_list_to = None
-        self.month_list_to = None
-        self.day_list_to = None
-        self.menu = None
-        self.submenu = None
-        self.style = None
         self.make_windows()
 
     def make_windows(self):
--- widgets.py
+++ widgets.py
@@ -4,6 +4,9 @@ class Widgets:
     kad matytų visą programą
     """
     def __init__(self):
+        self.topFrame = None
+        self.leftFrame = None
+
         self.materialsTable = None
         self.storageTable = None
         self.recipeTable = None
@@ -22,6 +25,7 @@ class Widgets:
         self.labelOrderProfit2 = None
         self.labelOrder = None
         self.labelRecipe = None
+        self.labelMailOrder = None
 
         self.entryFieldEdit1 = None
         self.entryFieldEdit2 = None
@@ -39,10 +43,16 @@ class Widgets:
         self.buttonCancelOrder = None
         self.buttonFilterOrders = None
         self.buttonCancelEditing = None
+        self.buttonMenu1 = None
+        self.buttonMenu2 = None
+        self.buttonMenu3 = None
+        self.buttonScrollUp = None
+        self.buttonScrollDown = None
 
         self.idForEdit = None
         self.scrollbar_recipe = None
         self.rates = None
+        self.pairs = None
         self.currency_list = None
         self.recipe_list = None
         self.mail_list = None
@@ -52,3 +62,6 @@ class Widgets:
         self.year_list_to = None
         self.month_list_to = None
         self.day_list_to = None
+        self.menu = None
+        self.submenu = None
+        self.style = None
